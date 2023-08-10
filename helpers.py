cd955028e0d49ac1a776f8004f0ffe126e24cc56
asardon <as@myso.finance>
2023-08-10 13:22:46 +0200

    init commit

---
+++ helpers.py
@@ -0,0 +1,97 @@
+import numpy as np
+from scipy.stats import norm
+from scipy import optimize
+
+def getD1(S, K, vol, dt, r, q):
+    return (np.log(S / K) + (r - q + vol**2 / 2) * dt) / \
+        (vol * np.sqrt(dt))
+
+def getCallPrice(spotPrice, strikePrice, vol, dt, riskFreeRate, q=0):
+    if dt <= 0:
+        return max([0, spotPrice - strikePrice])
+    if strikePrice <= 0:
+        return spotPrice
+    d1 = getD1(spotPrice, strikePrice, vol, dt, riskFreeRate, q)
+    d2 = d1 - vol * np.sqrt(dt)
+    value = np.exp(-q * dt) * spotPrice * norm.cdf(d1) - strikePrice * \
+        np.exp(-riskFreeRate * dt) * norm.cdf(d2)
+    return value
+
+def getFairApr(ltv, loanTenorInYears, spotPrice, vol, riskFreeRate, q=0):
+    def minFunc(strike):
+        y = spotPrice*ltv
+        callPrice = getCallPrice(spotPrice, strike, vol, loanTenorInYears, riskFreeRate, q)
+        z = spotPrice - callPrice - y
+        z = z * 100 if z < 10 else z # scale up for small prices
+        return z**2
+
+    step_size = 0.1  # 10% step size
+    upper_bound = spotPrice*10
+    lower_bound = .0000001
+
+    # Generate initial guesses
+    initStrikeGuesses_increasing = np.arange(spotPrice, upper_bound, spotPrice*step_size)
+    initStrikeGuesses_decreasing = np.arange(spotPrice, lower_bound, -spotPrice*step_size)
+    initStrikeGuesses = np.concatenate([initStrikeGuesses_decreasing, initStrikeGuesses_increasing])
+
+    strikeBnds = (lower_bound, None)
+
+    for initStrikeGuess in initStrikeGuesses:
+        res = optimize.minimize(
+            minFunc,
+            args=(),
+            x0=[initStrikeGuess],
+            bounds=[strikeBnds])
+        if res["success"] and res["fun"] < 0.001:
+            y = ltv*spotPrice
+            strike = res['x'][0]
+            apr = (strike/y-1)/loanTenorInYears
+            return apr
+    return None
+
+def getFairFee(ltv, loanTenorInYears, spotPrice, vol, riskFreeRate, q=0):
+    def minFunc(fee):   
+        y = spotPrice*ltv
+        callPrice = getCallPrice(spotPrice, y/(1-fee), vol, loanTenorInYears, riskFreeRate, q)
+        z = (1-fee)*(spotPrice - callPrice) - (y - fee*spotPrice)
+        
+        """
+        # old calculation methodology
+        callPrice = getCallPrice(spotPrice, y, vol, loanTenorInYears, riskFreeRate)
+        z = spotPrice - (1-fee)*callPrice - y
+        """
+        
+        z = z * 100 if z < 10 else z # scale up z for small prices
+        return z**2
+    
+    maxFee = .1
+    middleFee = maxFee / 2
+    feeInterval = middleFee / 5
+    
+    # Create a list of guesses starting from the middle of the range and adding guesses in alternating directions
+    initFeeGuesses = [middleFee + i * feeInterval * (-1) ** i for i in range(20)]
+    
+    for initFeeGuess in initFeeGuesses:
+        feeBnds = (.0, maxFee)
+        res = optimize.minimize(
+            minFunc,
+            args=(),
+            x0=[initFeeGuess],
+            bounds=[feeBnds])
+            
+        if res["success"] and res["fun"] < 0.001:
+            fee = res['x'][0]
+            return fee
+            
+    return None
+
+def generateQuoteTuple(ltv, spotPrice, tenorInYears, apr, upfrontFee, loanTokenDecimals, collTokenDecimals, withOracle=False):            
+    if withOracle:
+        loanPerCollUnitOrLtv = str(int(ltv * 10 ** 18))
+    else:
+        loanPerCollUnitOrLtv = str(int(ltv*spotPrice * 10 ** loanTokenDecimals))
+    interestRatePctInBase = str(int(apr * tenorInYears* 10 ** 18))
+    upfrontFeePctInBase = str(int(upfrontFee * 10 ** collTokenDecimals))
+    tenor = str(int(tenorInYears * 60 * 60 * 24 * 365))
+            
+    return ({"loanPerCollUnitOrLtv": loanPerCollUnitOrLtv, "interestRatePctInBase": interestRatePctInBase, "upfrontFeePctInBase": upfrontFeePctInBase, "tenor": tenor})
---
+++ main.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import helpers
+import matplotlib.pyplot as plt
+import traceback
+
+CMAP = 'plasma'
+LTVs = [.3, .4, .5, .6, .7, .8]
+TENORS = [30, 40, 50, 60, 70, 80, 90, 120, 180] # in days
+
+def get_user_input():
+    spot = st.sidebar.slider('Spot Price ($)', .01, 50000., 1.)
+    vol = st.sidebar.slider('Volatility (p.a.)', .01, 1.4, .5)
+    r = st.sidebar.slider('Interest rate (p.a.)', .0, .1, .0)
+    loan_token_decimals = st.sidebar.slider('Loan Token Decimals', 0, 18, 6)
+    coll_token_decimals = st.sidebar.slider('Coll. Token Decimals', 0, 18, 18)
+    return {'spot': spot, 'vol': vol, 'r': r, 'loan_token_decimals': loan_token_decimals, 'coll_token_decimals': coll_token_decimals}
+
+def get_heatmap(func, title, spot, vol, r):
+    fig, ax = plt.subplots()
+    heatmap_res = []
+
+    for ltv in LTVs:
+        tmp = []
+        for tenor in TENORS:
+            res = func(ltv, tenor/365, spot, vol, r)
+            tmp.append(res)
+        heatmap_res.append(tmp)
+    heatmap_res = np.array(heatmap_res, dtype=float)
+    heatmap_res = np.ma.array(heatmap_res, mask=np.isnan(heatmap_res))
+
+    cmap = plt.cm.get_cmap(CMAP)
+    cmap.set_bad('white',1.)
+    ax.imshow(heatmap_res, interpolation='nearest', cmap=CMAP)
+
+    normColors = plt.cm.colors.Normalize(vmin=np.min(heatmap_res), vmax=np.max(heatmap_res))
+    for i in range(len(LTVs)):
+        for j in range(len(TENORS)):
+            bg = cmap(normColors(heatmap_res[i][j]))
+            v = 0 if (bg[0] + bg[1] + bg[2]) / 3 > 0.5 else 1
+            c = (v, v, v, 1.)
+            ax.text(j, i, '{:.2f}%'.format(heatmap_res[i][j]*100),
+                    ha='center', va='center', color=c, fontsize='x-small')
+    
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(TENORS)), labels=['{:.0f}d'.format(x) for x in TENORS])
+    ax.set_yticks(np.arange(len(LTVs)), labels=['{:.0f}%'.format(x*100) for x in LTVs])
+    ax.invert_yaxis()
+
+    plt.title(title)
+    plt.xlabel('Tenor')
+    plt.ylabel('LTV')
+
+        
+    return heatmap_res, fig
+
+def get_raw_quote_tuples(heatmap_res, spot, loan_token_decimals, coll_token_decimals, with_oracle, is_apr):
+    quoteTuples = []
+    for i, ltv in enumerate(LTVs):
+        for j, tenor in enumerate(TENORS):
+            try:
+                apr = heatmap_res[i][j] if is_apr else 0
+                fee = heatmap_res[i][j] if not is_apr else 0
+                quoteTuple = helpers.generateQuoteTuple(ltv, spot, tenor/365, apr, fee, loan_token_decimals, coll_token_decimals, with_oracle)
+                quoteTuples.append(quoteTuple)
+            except Exception:
+                traceback.print_exc()
+    return quoteTuples
+
+st.title('Zero-Liquidation Loan Pricer')
+st.sidebar.header('Input assumptions:')
+
+user_input = get_user_input()
+
+apr_heatmap_res, apr_heatmap_fig = get_heatmap(helpers.getFairApr, 'APR Heatmap', user_input['spot'], user_input['vol'], user_input['r'])
+fee_heatmap_res, fee_heatmap_fig = get_heatmap(helpers.getFairFee, 'Fee Heatmap', user_input['spot'], user_input['vol'], user_input['r'])
+raw_apr_quote_tuples = get_raw_quote_tuples(apr_heatmap_res, user_input['spot'], user_input['loan_token_decimals'], user_input['coll_token_decimals'], False, True)
+raw_fee_quote_tuples = get_raw_quote_tuples(apr_heatmap_res, user_input['spot'], user_input['loan_token_decimals'], user_input['coll_token_decimals'], False, False)
+
+st.write(apr_heatmap_fig)
+st.subheader('JSON APR Quote Tuples', anchor=None, help=None)
+st.json(raw_apr_quote_tuples, expanded=False)
+
+st.write(fee_heatmap_fig)
+st.subheader('JSON Fee Quote Tuples', anchor=None, help=None)
+st.json(raw_fee_quote_tuples, expanded=False)
